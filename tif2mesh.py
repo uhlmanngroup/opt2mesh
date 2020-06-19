b3167b7cf7e470112247ecc5a839ed3cdb28e9b3
Julien Jerphanion (jerphanion) <jerphanion@ebi.ac.uk>
2020-06-19 14:35:46 +0100

    [WIP] Trying sobel filter + flooding
    This adds some fixtures to try to extract contours using
    sobol filter and some flooding.
    
    In this context, the main parameter one can tune is the
    tolerance.
    
    Results are quite noisy, we might want to use something then
    to extract the contour. Maybe the active contour technique
    might work nicely in this context.
    
    Next:
     - try to use some active contour techniques to extract
     the shape of the embryo
     - directly try marching cubes techniques

--- tif2mesh.py
+++ tif2mesh.py
@@ -4,6 +4,8 @@ import itertools
 import matplotlib.pyplot as plt
 from skimage import io, feature
 from skimage import filters
+from skimage.filters import gaussian
+from skimage.segmentation import flood, flood_fill
 from joblib import Parallel, delayed
 
 
@@ -31,22 +33,70 @@ def extract_canny(image, i, gaussian_blur_sigma=1, sigma=20):
     out_folder = f"/tmp/canny/{sigma}"
 
     os.makedirs(out_folder, exist_ok=True)
-    fig.savefig(f"{out_folder}/{i}.png")
+    fig.savefig(f"{out_folder}/" + f"{i}".zfill(4) + ".png")
     plt.close(fig)
 
 
-if __name__ == "__main__":
-    tiffile = "/tmp/MNS_M897_115.tif"
+def extract_sobel_flood(image, i, gaussian_blur_sigma=1, tolerance=2):
+    slice = filters.gaussian(image, gaussian_blur_sigma)
 
-    parallel = Parallel(n_jobs=2)
+    print(i)
+    h, w = slice.shape
 
-    opt_data = io.imread(tiffile).astype("float64")
+    img_sobel = filters.sobel(slice)
+    mask = flood(slice, (0, 0), tolerance=tolerance)
+
+    fig, ax = plt.subplots(ncols=3, figsize=(13, 8))
+
+    ax[0].imshow(slice)
+    ax[0].set_title(f'Slice {i}')
+    ax[0].axis('off')
+
+    ax[1].imshow(img_sobel)
+    ax[1].set_title('Sobel filtered')
+    ax[1].axis('off')
+
+    ax[2].imshow(mask, cmap=plt.cm.gray)
+    ax[2].set_title(f'Segmented tolerance={tolerance}')
+    ax[2].axis('off')
+
+    out_folder = f"/tmp/sobel_flood/{tolerance}"
+
+    os.makedirs(out_folder, exist_ok=True)
+    fig.savefig(f"{out_folder}/" + f"{i}".zfill(4) + ".png")
+    plt.close(fig)
+
+
+def canny_filter():
+    parallel = Parallel(n_jobs=3)
+
+    sigmas = [5, 10, 15, 20, 25]
 
     with parallel:
-        sigmas = [10, 15, 20, 25]
         args = itertools.product(list(range(511)), sigmas)
 
         parallel(delayed(extract_canny)(opt_data[i], i,
                                         gaussian_blur_sigma=1,
                                         sigma=sigma)
                  for (i, sigma) in args)
+
+
+def sobel_flood():
+    parallel = Parallel(n_jobs=3)
+
+    tolerances = [1.5, 2, 5]
+
+    with parallel:
+        args = itertools.product(list(range(511)), tolerances)
+
+        parallel(delayed(extract_sobel_flood)(opt_data[i], i,
+                                              gaussian_blur_sigma=1,
+                                              tolerance=tolerance)
+                 for (i, tolerance) in args)
+
+
+if __name__ == "__main__":
+    tiffile = "/tmp/MNS_M897_115.tif"
+
+    opt_data = io.imread(tiffile).astype("float64")
+    sobel_flood()
