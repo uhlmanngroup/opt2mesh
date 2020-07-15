8d9428c236d0f00bc6bb33a4a0bac204340a1040
Julien Jerphanion (jerphanion) <jerphanion@ebi.ac.uk>
2020-07-15 11:43:56 +0100

    Add a simple submitter to submit jobs using a file
    One of the problem with jobs submission is:
    
     - the tracking of jobs
     - potential mistakes made on the command line
     - the fact that jobs are run in batch on a set of parameters
     - a description of jobs to make sense of this batch in the history
    
    This introduce a really simple script to submit jobs with a
    YAML description of them.
    
    This might help with inspecting results as allows solving the
    different problems stated above.
    
    The formatting has to be enforced: this script does not keep
    track of eventual mispells on YAML keys, yet.

--- submit.py
+++ submit.py
@@ -1,96 +1,102 @@
 #!/usr/bin/env python
+import os
 import sys
 import yaml
-import argparse
+import itertools
+from collections import ChainMap
+
+__doc__ = """
+
+    Job submitter on a yaml request specified like this.
+
+    Example:
+
+    ```yaml
+    batch_job_name: crf
+    goal: This batch of jobs is dedicated to explore different values \n
+          of parameters for CRF on two different input files.\n\n
+          This would results in 32 total jobs.
+    python_exec: /homes/jerphanion/.miniconda3/envs/ec/bin/python
+    file: /homes/jerphanion/mesh-processing-pipeline/src/python/crf.py
+    memory: 20000
+    cpus: 4
+    input_files:
+      - /homes/jerphanion/mesh-processing-pipeline/data/original/MNS_M897_115.tif
+      - /homes/jerphanion/mesh-processing-pipeline/data/original/MNS_M851_12.tif
+    output_folder: /homes/jerphanion/mesh-processing-pipeline/out/crf
+    options:
+      - max_iter: [2, 3]
+      - std_pos: [0.2, 0.5]
+      - weight_pos: [0.7, 1.0]
+      - std_bilat: [3.0, 5.0]
+      - weight_bilat: [15.0]
+    ```
+"""
 
 
-class ConfigParser(object):
-    def __init__(self, *pargs, **kwpargs):
-        self.options = []
-        self.pargs = pargs
-        self.kwpargs = kwpargs
+def bsub_command(python_exec, file, batch_job_name,
+                 memory, cpus, input_file, output_folder, options):
+    options_str = ""
+    for k, v in options.items():
+        options_str += f" --{k} {v} \ \n"
 
-    def add(self, *args, **kwargs):
-        self.options.append((args, kwargs))
-
-    def parse(self, args=None):
-        if args is None:
-            args = sys.argv[1:]
-
-        conf_parser = argparse.ArgumentParser(add_help=False)
-        conf_parser.add_argument("-c", "--config",
-                                 default=None,
-                                 help="where to load YAML configuration",
-                                 metavar="FILE")
-
-        res, remaining_argv = conf_parser.parse_known_args(args)
-
-        config_vars = {}
-        if res.config is not None:
-            with open(res.config, 'r') as stream:
-                config_vars = yaml.load(stream)
-
-        parser = argparse.ArgumentParser(
-            *self.pargs,
-            # Inherit options from config_parser
-            parents=[conf_parser],
-            # Don't mess with format of description
-            formatter_class=argparse.RawDescriptionHelpFormatter,
-            **self.kwpargs,
-        )
-
-        for opt_args, opt_kwargs in self.options:
-            parser_arg = parser.add_argument(*opt_args, **opt_kwargs)
-            if parser_arg.dest in config_vars:
-                config_default = config_vars.pop(parser_arg.dest)
-                expected_type = str
-                if parser_arg.type is not None:
-                    expected_type = parser_arg.type
-
-                if not isinstance(config_default, expected_type):
-                    parser.error('YAML configuration entry {} '
-                                 'does not have type {}'.format(
-                                     parser_arg.dest,
-                                     expected_type))
-
-                parser_arg.default = config_default
-
-        if config_vars:
-            parser.error('unexpected configuration entries: ' + \
-                         ', '.join(config_vars))
-
-        return parser.parse_args(remaining_argv)
+    command = "bsub \ \n"
+    command += f" -J {batch_job_name} \ \n"
+    command += f" -M {memory} \ \n"
+    command += f' -R "rusage[mem={memory}]" \ \n'
+    command += f' -n {cpus} \ \n'
+    command += f'{python_exec} \ \n'
+    command += f'{file} \ \n'
+    command += f'{options_str}'
+    command += f'{input_file} \ \n'
+    command += f'{output_folder} \ \n'
+    return command
 
 
 if __name__ == "__main__":
-    cp = ConfigParser()
+    yaml_file = sys.argv[1]
+    with open(yaml_file, 'r') as stream:
+        try:
+            command = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
 
-    cp.add("python_exec")
-    cp.add("file")
-    cp.add("memory", type=int)
-    cp.add("cpus", type=int)
-    cp.add("parameters")
-    print(cp.parse())
+    python_exec = command["python_exec"]
+    file = command["file"]
+    batch_job_name = command["batch_job_name"]
+    memory = command["memory"]
+    cpus = command["cpus"]
+    inputs_list = command["input_files"]
+    output_folder = command["output_folder"]
 
-# yaml_file = sys.argv[1]
-# with open(yaml_file, 'r') as stream:
-#     try:
-#         command = yaml.safe_load(stream)
-#     except yaml.YAMLError as exc:
-#         print(exc)
+    options_set = command["options"]
 
-#     python_env = command["env"]
-#     file = command["file"]
-#     batch_job_name = command["batch_job_name"]
-#     memory = command["memory"]
-#     cpus = command["cpus"]
-#     parameters:
-#     - / homes / jerphanion / mesh - processing - pipeline / data / original / MNS_M897_115.tif
-#     - / homes / jerphanion / mesh - processing - pipeline / out / crf
-#
-# options:
-# - max_iter: [2]
-# - std_pos: [3.0]
-# - weight_pos: [10.0]
-# - std_bilat: [3.0]
-# - weight_bilat: [15.0]
+    # Merging dict together
+    options_set = dict(ChainMap(*options_set))
+
+    options_keys = options_set.keys()
+    options_vals = list(options_set.values())
+
+    bsub_commands = []
+
+    # Creating commands for each files
+    for input_file in inputs_list:
+
+        # Submitting all the jobs possible on the cartesian product of options
+        for values in itertools.product(*options_vals):
+            options = dict(zip(options_keys, values))
+
+            std_out_command = bsub_command(python_exec,
+                                           file,
+                                           batch_job_name,
+                                           memory,
+                                           cpus,
+                                           input_file,
+                                           output_folder,
+                                           options)
+
+            bsub_commands.append(std_out_command)
+
+    for i, command in enumerate(bsub_commands):
+        print(i)
+        std_out_command = os.popen(f"{command}").read()
