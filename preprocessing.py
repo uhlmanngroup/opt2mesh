aca39e5c5e5cecb01dd27d9500c3681be31b08c3
Julien Jerphanion (jerphanion) <jerphanion@ebi.ac.uk>
2020-07-17 10:19:26 +0100

    Adapt preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -23,6 +23,9 @@ from matplotlib import pyplot as plt
 def denoise_nl_means(slice):
     """
     Simple adaptation for parallelization.
+
+    Taken and adapted from the documentation.
+
     """
     sigma_est = np.mean(estimate_sigma(slice))
 
@@ -39,7 +42,28 @@ def denoise_nl_means(slice):
     return slice_denoised
 
 
+def _log_nd_array_info(data: np.ndarray):
+    logging.info(f"Shape: {data.shape}")
+    logging.info(f"dtype: {data.dtype}")
+    logging.info(f"Min:   {data.min()}")
+    logging.info(f"Max:   {data.max()}")
+    logging.info(f"Median:{np.median(data)}")
+    logging.info(f"Mean  :{data.mean()}")
+
+
 def __parallel_denoising(joblib_parallel, opt_data, denoise_function, method):
+    """
+    Denoise a TIF stack a slice at a time, in a parallel fashion.
+
+    This is not optimal: the denoising should be done on the entire stack
+    but we need to have kind a lot of memory for this.
+
+    @param joblib_parallel:
+    @param opt_data:
+    @param denoise_function:
+    @param method:
+    @return:
+    """
     logging.info(f"Starting {method}")
     denoised_opt_data = joblib_parallel(delayed(denoise_function)(img)
                                         for img in opt_data)
@@ -50,79 +74,100 @@ def __parallel_denoising(joblib_parallel, opt_data, denoise_function, method):
     return denoised_opt_data
 
 
-def downsample(opt_data, file_basename):
+def downsample(opt_data, file_basename, joblib_parallel=None):
     opt_data_downsampled = opt_data[::2, ::2, ::2]
     filename = file_basename + "_downsampled.tif"
     logging.info(f"Saving at {filename} (shape: {opt_data_downsampled.shape})")
     io.imsave(filename, opt_data_downsampled)
 
 
-def denoise(opt_data, file_basename):
-    with Parallel(n_jobs=4, backend="loky") as parallel:
+def denoise(opt_data, file_basename, joblib_parallel=None):
+    if joblib_parallel is not None:
+        with joblib_parallel:
+            filename = file_basename + "_tv_denoised.tif"
+            denoised_opt_data = __parallel_denoising(joblib_parallel, opt_data,
+                                                     method="TV-L1 denoising (Chambolle pock)",
+                                                     denoise_function=restoration.denoise_tv_chambolle)
+            logging.info(f"Saving at {filename} (shape: {denoised_opt_data.shape})")
+            io.imsave(filename, denoised_opt_data)
+
+            filename = file_basename + "_median_denoised.tif"
+            denoised_opt_data = __parallel_denoising(joblib_parallel, opt_data,
+                                                     method="median filtering",
+                                                     denoise_function=filters.median)
+            logging.info(f"Saving at {filename} (shape: {denoised_opt_data.shape})")
+            io.imsave(filename, denoised_opt_data)
+
+            filename = file_basename + "_nl_means_denoised.tif"
+            denoised_opt_data = __parallel_denoising(joblib_parallel, opt_data,
+                                                     method="non linear means denoising",
+                                                     denoise_function=denoise_nl_means)
+            logging.info(f"Saving at {filename} (shape: {denoised_opt_data.shape})")
+            io.imsave(filename, denoised_opt_data)
+    else:
         filename = file_basename + "_tv_denoised.tif"
-        denoised_opt_data = __parallel_denoising(parallel, opt_data,
-                                                 method="TV-L1 denoising (Chambolle pock)",
-                                                 denoise_function=restoration.denoise_tv_chambolle)
+        denoised_opt_data = restoration.denoise_tv_chambolle(opt_data,
+                                                             multichannel=False)
         logging.info(f"Saving at {filename} (shape: {denoised_opt_data.shape})")
         io.imsave(filename, denoised_opt_data)
 
         filename = file_basename + "_median_denoised.tif"
-        denoised_opt_data = __parallel_denoising(parallel, opt_data,
-                                                 method="median filtering",
-                                                 denoise_function=filters.median)
+        denoised_opt_data = filters.median(opt_data)
         logging.info(f"Saving at {filename} (shape: {denoised_opt_data.shape})")
         io.imsave(filename, denoised_opt_data)
 
         filename = file_basename + "_nl_means_denoised.tif"
-        denoised_opt_data = __parallel_denoising(parallel, opt_data,
-                                                 method="non linear means denoising",
-                                                 denoise_function=denoise_nl_means)
+        denoised_opt_data = denoise_nl_means(opt_data)
         logging.info(f"Saving at {filename} (shape: {denoised_opt_data.shape})")
         io.imsave(filename, denoised_opt_data)
 
 
-def contrast(opt_data, file_basename):
-    def log_info(data: np.ndarray):
-        logging.info(f"Shape: {data.shape}")
-        logging.info(f"dtype: {data.dtype}")
-        logging.info(f"Min:   {data.min()}")
-        logging.info(f"Max:   {data.max()}")
-        logging.info(f"Median:{np.median(data)}")
-        logging.info(f"Mean  :{data.mean()}")
+def contrast(opt_data: np.ndarray, file_basename: str, joblib_parallel=None):
+    """
+    Perform contrast rectification and save results.
 
+    The contrast equalization methods that are used are:
+        - Intensity Rescaling
+        - Histogram Equalization
+        - Contrast Limited Adaptive Histogram Equalization
+
+    Taken and adapted from the documentation.
+    """
     logging.info("Performing contrast equalization")
 
     logging.info("Original data")
-    log_info(opt_data)
+    _log_nd_array_info(opt_data)
 
-    logging.info("Contrast stretching")
+    logging.info("Intensity Rescaling")
     p2, p98 = np.percentile(opt_data, (2, 98))
     opt_data_rescale = exposure.rescale_intensity(opt_data, in_range=(p2, p98))
-    log_info(opt_data_rescale)
+    _log_nd_array_info(opt_data_rescale)
     filename = file_basename + "_rescale.tif"
     logging.info(f"Saving at {filename} (shape: {opt_data_rescale.shape})")
     io.imsave(filename, opt_data_rescale)
 
-    logging.info("Equalization")
+    logging.info("Histogram Equalization")
     opt_data_eq = exposure.equalize_hist(opt_data)
-    log_info(opt_data_eq)
+    _log_nd_array_info(opt_data_eq)
     opt_data_eq = (opt_data_eq * 255).astype(np.uint8)
-    log_info(opt_data_eq)
+    _log_nd_array_info(opt_data_eq)
     filename = file_basename + "_eq.tif"
     logging.info(f"Saving at {filename} (shape: {opt_data_eq.shape})")
     io.imsave(filename, opt_data_eq)
 
-    logging.info("Adaptive Equalization")
+    logging.info("Contrast Limited Adaptive Histogram Equalization")
     opt_data_adapt_eq = exposure.equalize_adapthist(opt_data, clip_limit=0.03)
-    log_info(opt_data_adapt_eq)
+    _log_nd_array_info(opt_data_adapt_eq)
     filename = file_basename + "_adapt_eq.tif"
     logging.info(f"Saving at {filename} (shape: {opt_data_adapt_eq.shape})")
     io.imsave(filename, opt_data_adapt_eq)
 
 
-def extract_png(opt_data, file_basename):
+def extract_png(opt_data, file_basename, joblib_parallel=None):
     """
     Extract all the slices and save them as png images.
+
+    Typically used for visual inspection.
     """
     for slice_index, slice in enumerate(opt_data):
         logging.info(f"Extracting slice {slice_index} as a png image")
@@ -134,9 +179,11 @@ def extract_png(opt_data, file_basename):
         plt.close('all')
 
 
-def extract_tif(opt_data, file_basename):
+def extract_tif(opt_data, file_basename, joblib_parallel=None):
     """
     Extract all the slices and save them as tif images.
+
+    Typically used when using Ilastik.
     """
     for slice_index, slice in enumerate(opt_data):
         logging.info(f"Extracting slice {slice_index} as a tif image")
@@ -164,7 +211,7 @@ def parse_args():
     parser.add_argument("out_folder", help="General output folder for this run")
 
     parser.add_argument("--n_jobs", help="Number of parallel jobs",
-                        type=int, default=4)
+                        type=int, default=0)
 
     return parser.parse_args()
 
@@ -188,10 +235,12 @@ def main():
         ]
     )
 
+    joblib_parallel = Parallel(n_jobs=args.n_jobs, backend="loky") if args.n_jobs > 0 else None
+
     file_basename = os.path.join(args.out_folder, basename)
     processing = commands[args.step]
 
-    processing(opt_data, file_basename)
+    processing(opt_data, file_basename, joblib_parallel)
 
 
 if __name__ == "__main__":
