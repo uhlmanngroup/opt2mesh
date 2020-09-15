83b2c2217932adc638144bde3c9a395106e91549
Julien Jerphanion (jerphanion) <jerphanion@ebi.ac.uk>
2020-09-15 15:06:15 +0100

    [WIP] Script for ground truth construction

--- scripts/binarize.py
+++ scripts/binarize.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python
+
 from skimage import io
 import argparse
 import numpy as np
@@ -5,16 +7,35 @@ import numpy as np
 __doc__ = "Binarise a tif stack based on a decision threshold" \
           "used to construct the ground truth."
 
+from skimage.morphology import flood_fill
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("tif_file", help="TIF stack")
-    parser.add_argument("threshold",
+    parser.add_argument("threshold", type=float,
                         help="Value in [0, 1] to binarise image")
 
     args = parser.parse_args()
 
     data = io.imread(args.tif_file)
-    data_out = np.array(data > args.threshold, dtype=np.uint8)
-    io.imsave(args.tif_file.replace(".tif", "bin.tif"), data_out)
+    data_out = np.array(data > args.threshold * 255, dtype=np.uint8)
+    data_out = flood_fill(data_out, (1, 1, 1), 2)
+    data_out_new = np.array(data_out != 2, dtype=np.uint8)
+
+    # binarize MNS_M059_105_clahe_median_denoised_occupancy_map.tif 0.75
+    # data_out_new[200:400, 512-200:512-110, 180:300] = 2
+
+    # binarize MNS_M090_115a_clahe_median_denoised_occupancy_map.tif 0.85
+    # data_out_new[225:275, 512-350:512-300, 300:350] = 2
+    # data_out_new[150:200, 512-320:512-200, 280:350] = 2
+
+    # binarize MNS_M173a_115_clahe_median_denoised_occupancy_map.tif 0.85
+    # data_out_new[300:440, 512-275:512-220, 125:375] = 2
+    # data_out_new[190:210, 512-170:512-140, 225:300] = 2
+
+    # binarize MNS_M395a_115_clahe_median_denoised_occupancy_map.tif 0.85
+    # data_out_new[120:210, 512-375:512-220, 300:375] = 2
+
+    io.imsave(args.tif_file.replace(".tif", "_bin.tif"), data_out_new)
 
---
+++ scripts/merge.py
@@ -0,0 +1,63 @@
+#! /usr/bin/env python
+
+import sys
+import argparse
+from skimage import io
+import h5py
+import os
+import numpy as np
+
+
+
+__doc__ = "Merge examples and their labels together in the 3D UNet format"
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument("examples_folder")
+    parser.add_argument("predictions_folder")
+    parser.add_argument("out_folder")
+    parser.add_argument("--prediction_level", help="Decision threshold for"
+            "binarization", type=float, default=0.85)
+
+    args = parser.parse_args()
+
+    examples_files = sorted(os.listdir(args.examples_folder))
+    predictions_files  = sorted(os.listdir(args.predictions_folder))
+
+    os.makedirs(args.out_folder, exist_ok=True)
+
+    for ex_f, prediction_f in zip(examples_files, predictions_files):
+        ex = io.imread(os.path.join(args.examples_folder, ex_f))
+        prediction = io.imread(os.path.join(args.predictions_folder,
+            prediction_f))
+
+        print("Processing", ex_f, "and", prediction_f)
+
+        assert 0 <= ex.min(), "Should be in [[0, 255]]"
+        assert ex.max() <= 255, "Should be in [[0, 255]]"
+
+        assert 0 <= prediction.min(), "Should be in [[0, 255]]"
+        assert prediction.max() <= 255, "Should be in [[0, 255]]"
+
+        # As examples are 511 × 512 × 512
+        # We crop then to have the same shape on different axis
+        first, last = 0, 511
+        ex = ex[first:last, first:last, first:last]
+
+        labels = (prediction[first:last, first:last, first:last] / 255) > \
+            args.prediction_level
+
+        labels = np.array(labels, dtype=np.uint8)
+
+        out_file = os.path.join(args.out_folder, ex_f.replace(".tif",
+            "_full.h5"))
+
+        hf = h5py.File(out_file, "w")
+        hf.create_dataset(name="raw", data=ex, chunks=True)
+        hf.create_dataset(name="labels", data=labels, chunks=True)
+        hf.close()
+
+        print(f"Saved the example in {out_file}")
+
