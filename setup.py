0f4dcc5c026aa5e300f56ae85d38275559c5070d
Julien Jerphanion (jerphanion) <jerphanion@ebi.ac.uk>
2020-10-21 11:20:02 +0100

    Format code with black

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@
 
 from setuptools import setup
 
-setup(name="opt2mesh",
-      version="0.1.0",
-      description="Extract meshes for OPT scan.",
-      author="Julien Jerphanion",
-      author_email="jerphanion@ebi.ac.uk",
-      entry_points={
-            "console_scripts": ['opt2mesh = opt2mesh:main']
-      },
-      packages=["unet3d", "unet3d", "unet3d.datasets", "unet", "pipeline"]
-      )
+setup(
+    name="opt2mesh",
+    version="0.1.0",
+    description="Extract meshes for OPT scan.",
+    author="Julien Jerphanion",
+    author_email="jerphanion@ebi.ac.uk",
+    entry_points={"console_scripts": ["opt2mesh = opt2mesh:main"]},
+    packages=["unet3d", "unet3d", "unet3d.datasets", "unet", "pipeline"],
+)
