8470f9c984c4ad25c2f7231678287f3c76d9e061
Julien Jerphanion (jerphanion) <jerphanion@ebi.ac.uk>
2020-10-27 10:41:07 +0000

    Add tests for the DirectMeshingPipeline

--- tests/test_run.py
+++ tests/test_run.py
@@ -11,9 +11,36 @@ models_dir = os.path.join(root_dir, "models")
 
 
 def test_direct_meshing():
+    """ DirectMeshingPipeline default settings should run. """
     input_file_binary = os.path.join(test_data_dir, "MNS_M539_105_binary.tif")
     with tempfile.TemporaryDirectory() as tmp:
         pipeline = DirectMeshingPipeline()
         pipeline.run(input_file_binary, tmp)
 
 
+def test_direct_meshing_save_temp():
+    """ Temporary artifacts should be saved. """
+    input_file_binary = os.path.join(test_data_dir, "MNS_M539_105_binary.tif")
+    with tempfile.TemporaryDirectory() as tmp:
+        pipeline = DirectMeshingPipeline(save_temp=True)
+        pipeline.run(input_file_binary, tmp)
+        extracted_mesh_file = os.path.join(
+            tmp, "MNS_M539_105_binary_extracted_mesh.stl"
+        )
+        assert os.path.isfile(
+            extracted_mesh_file
+        ), "The extracted mesh is not present after having run the pipeline."
+
+
+def test_direct_meshing_save_occupancy_map():
+    """ The occupancy map should be saved. """
+    input_file_binary = os.path.join(test_data_dir, "MNS_M539_105_binary.tif")
+    with tempfile.TemporaryDirectory() as tmp:
+        pipeline = DirectMeshingPipeline(save_occupancy_map=True)
+        pipeline.run(input_file_binary, tmp)
+        occupancy_map_file = os.path.join(
+            tmp, "MNS_M539_105_binary_extracted_mesh.stl"
+        )
+        assert os.path.isfile(
+            occupancy_map_file
+        ), "The occupancy map is not present after having run the pipeline."
