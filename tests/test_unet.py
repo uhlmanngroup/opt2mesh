769066ecb8e55c4cd1caefba52bd314ade215461
Julien Jerphanion (jerphanion) <jerphanion@ebi.ac.uk>
2020-10-27 18:02:31 +0000

    Adapt tests for UNet pipelines

--- tests/test_unet.py
+++ tests/test_unet.py
@@ -10,18 +10,31 @@ root_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)
 models_dir = os.path.join(root_dir, "models")
 
 
-
 def test_3dunet():
+    """ UNet3DPipeline default settings should run and output the mesh. """
     input_file = os.path.join(test_data_dir, "MNS_M539_105_preprocessed.tif")
     model_file = os.path.join(models_dir, "3d_preprocessed.pytorch")
     with tempfile.TemporaryDirectory() as tmp:
         pipeline = UNet3DPipeline(model_file=model_file)
         pipeline.run(input_file, tmp)
+        final_mesh_file = os.path.join(
+            tmp, "MNS_M539_105_binary_final_mesh.tif"
+        )
+        assert os.path.isfile(
+            final_mesh_file
+        ), "The final mesh is not present in the results."
 
 
 def test_2dunet():
+    """ UNetPipeline default settings should run and output the mesh. """
     input_file = os.path.join(test_data_dir, "MNS_M539_105_preprocessed.tif")
     model_file = os.path.join(models_dir, "2d_preprocessed.pytorch")
     with tempfile.TemporaryDirectory() as tmp:
         pipeline = UNetPipeline(model_file=model_file)
         pipeline.run(input_file, tmp)
+        final_mesh_file = os.path.join(
+            tmp, "MNS_M539_105_binary_final_mesh.tif"
+        )
+        assert os.path.isfile(
+            final_mesh_file
+        ), "The final mesh is not present in the results."
