d9b64b5fecde92c6c79287ef011dfddaa018d5dc
Julien Jerphanion (jerphanion) <jerphanion@ebi.ac.uk>
2020-11-03 14:37:12 +0000

    Add tests for UNet

--- tests/test_unet.py
+++ tests/test_unet.py
@@ -1,6 +1,9 @@
 import os
 import tempfile
 
+import igl
+import pytest
+
 from pipeline.unet import UNet3DPipeline, UNetPipeline
 
 test_dir = os.path.dirname(os.path.realpath(__file__))
@@ -38,3 +41,36 @@ def test_2dunet():
         assert os.path.isfile(
             final_mesh_file
         ), "The final mesh is not present in the results."
+
+
+def test_direct_meshing_save_occupancy_map():
+    """ The occupancy map should be saved. """
+    input_file = os.path.join(test_data_dir, "MNS_M539_105_preprocessed.tif")
+    model_file = os.path.join(models_dir, "2d_preprocessed.pytorch")
+    with tempfile.TemporaryDirectory() as tmp:
+        pipeline = UNetPipeline(model_file=model_file, save_occupancy_map=True)
+        pipeline.run(input_file, tmp)
+        occupancy_map_file = os.path.join(
+            tmp, "MNS_M539_105_preprocessed_occupancy_map.tif"
+        )
+        assert os.path.isfile(
+            occupancy_map_file
+        ), "The occupancy map is not present after having run the pipeline."
+
+
+@pytest.mark.parametrize("detail", [3000, 10000, 20000])
+def test_direct_meshing_detail_str(detail):
+    """The direct meshing pipeline should work on various level of details.
+    If the detail are integers, output meshes should have less
+    faces than the indicated detail."""
+    input_file = os.path.join(test_data_dir, "MNS_M539_105_preprocessed.tif")
+    model_file = os.path.join(models_dir, "2d_preprocessed.pytorch")
+    with tempfile.TemporaryDirectory() as tmp:
+        pipeline = UNetPipeline(model_file=model_file, save_occupancy_map=True)
+        pipeline.run(input_file, tmp)
+        final_mesh_file = os.path.join(
+            tmp, "MNS_M539_105_preprocessed_final_mesh.stl"
+        )
+        v, f = igl.read_triangle_mesh(final_mesh_file)
+
+        assert len(f) <= detail
